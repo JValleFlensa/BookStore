e65eaa357c2b32c87412c55c6c464173ce97bb9a
Julio Valle <jvalle@flensa.com.mx>
2024-02-29 14:02:58 -0600

    ╭∩╮( •̀_•́ )╭∩╮

--- BookStore.Web/Controllers/CategoryController.cs
+++ BookStore.Web/Controllers/CategoryController.cs
@@ -1,76 +1,118 @@
-﻿using BookStore.Web.Data;
-using BookStore.Web.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿    using BookStore.Web.Data;
+    using BookStore.Web.Models;
+    using Microsoft.AspNetCore.Mvc;
 
-namespace BookStore.Web.Controllers
-{
-    public class CategoryController : Controller
+    namespace BookStore.Web.Controllers
     {
-        private readonly ApplicationDbContext _context;
-
-        public CategoryController(ApplicationDbContext context) { 
-            _context = context;
-        }
-
-        public IActionResult Index()
+        public class CategoryController : Controller
         {
-            IEnumerable<Category> categoryList = _context.Categories.ToList();
-            return View(categoryList);
-        }
+            private readonly ApplicationDbContext _context;
 
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(Category category)
-        {
-            if (category.Name == category.DisplayOrder.ToString()) {
-                ModelState.AddModelError("Name", "El nombre de la categoría no puede ser el mismo al del orden!");
+            public CategoryController(ApplicationDbContext context) { 
+                _context = context;
             }
 
-            if (ModelState.IsValid)
+            public IActionResult Index()
             {
-                _context.Categories.Add(category);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
-
-            }
-            return View(category);
-        }
-
-        public IActionResult Edit(int id)
-        {
-            Category category = _context.Categories.FirstOrDefault(c => c.Id == id)!;
-            if (category == null) 
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Category category)
-        {
-            if (category.Name == category.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("Name", "El nombre de la categoría no puede ser el mismo al del orden!");
+                IEnumerable<Category> categoryList = _context.Categories.ToList();
+                return View(categoryList);
             }
 
-            if (ModelState.IsValid)
+            public IActionResult Create()
             {
-                Category updatedCategory = _context.Categories.FirstOrDefault(c => c.Id == category.Id)!;
-                if (updatedCategory != null)
-                {
-                    updatedCategory.Name = category.Name;
-                    updatedCategory.DisplayOrder = category.DisplayOrder;
-                    _context.Categories.Update(updatedCategory);
-                    _context.SaveChanges();
-                    return RedirectToAction(nameof(Index));
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Create(Category category)
+            {
+                if (category.Name == category.DisplayOrder.ToString()) {
+                    ModelState.AddModelError("Name", "El nombre de la categoría no puede ser el mismo al del orden!");
                 }
+
+                if (ModelState.IsValid)
+                {
+                    _context.Categories.Add(category);
+                    _context.SaveChanges();
+
+                TempData["success"] = "Categoria creada satisfactoriamente";
+                
+                    return RedirectToAction(nameof(Index));
+
+                }
+                return View(category);
             }
-            return View(category);
+
+            public IActionResult Edit(int id)
+            {
+                Category category = _context.Categories.FirstOrDefault(c => c.Id == id)!;
+                if (category == null) 
+                {
+                    return NotFound();
+                    //return RedirectToAction(nameof(Index));
+                }
+                return View(category);
+            }
+
+            [HttpPost]
+            public IActionResult Edit(Category category)
+            {
+                if (category.Name == category.DisplayOrder.ToString())
+                {
+                    ModelState.AddModelError("Name", "El nombre de la categoría no puede ser el mismo al del orden!");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    //Category updatedCategory = _context.Categories.FirstOrDefault(c => c.Id == category.Id)!;
+                    //if (updatedCategory != null)
+                    //{
+                    //    updatedCategory.Name = category.Name;
+                    //    updatedCategory.DisplayOrder = category.DisplayOrder;
+                    //    _context.Categories.Update(updatedCategory);
+                    //    _context.SaveChanges();
+                    //    return RedirectToAction(nameof(Index));
+                    //}
+
+
+                    if (category != null && category.Id != 0)
+                    {
+                        _context.Update(category);
+                        _context.SaveChanges();
+
+                    TempData["success"] = "Categoria modificada satisfactoriamente";
+
+                    return RedirectToAction(nameof(Index));
+                    }
+                }
+                return View(category);
+            }
+
+            public IActionResult Delete(int id)
+            {
+                Category category = _context.Categories.FirstOrDefault(c => c.Id == id)!;
+                if (category == null)
+                {
+                    return NotFound();
+                    //return RedirectToAction(nameof(Index));
+                }
+                return View(category);
+            }
+
+
+            [HttpPost, ActionName("Delete")]
+            public IActionResult DeletePost(int id)
+            {
+
+                Category category = _context.Categories.FirstOrDefault(c => c.Id == id)!;
+                if (category != null)
+                {
+                    _context.Remove(category);
+                    _context.SaveChanges();
+                TempData["success"] = "Categoria eliminada satisfactoriamente";
+                return RedirectToAction(nameof(Index));
+                }
+                return View(category);
+            }
         }
     }
-}
--- BookStore.Web/wwwroot/lib/jquery-validation/dist/jquery.validate.js
+++ BookStore.Web/wwwroot/lib/jquery-validation/dist/jquery.validate.js
@@ -382,8 +382,8 @@ $.extend( $.validator, {
 		minlength: $.validator.format( "Please enter at least {0} characters." ),
 		rangelength: $.validator.format( "Please enter a value between {0} and {1} characters long." ),
 		range: $.validator.format( "Please enter a value between {0} and {1}." ),
-		max: $.validator.format( "Please enter a value less than or equal to {0}." ),
-		min: $.validator.format( "Please enter a value greater than or equal to {0}." ),
+		max: $.validator.format( "Ingrese un valor no mayor a {0}." ),
+		min: $.validator.format( "Ingrese un valor no menor a {0}." ),
 		step: $.validator.format( "Please enter a multiple of {0}." )
 	},
 
